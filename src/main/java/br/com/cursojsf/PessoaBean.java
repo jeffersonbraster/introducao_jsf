851c8e8a54a0aeb950aacf1c59e7e1c3d1262c1b
Jefferson Brandao <jeffersonbraster@gmail.com>
2019-09-04 17:23:26 -0300

    realizando testes com jsf e hibernate

--- src/main/java/br/com/cursojsf/PessoaBean.java
+++ src/main/java/br/com/cursojsf/PessoaBean.java
@@ -1,14 +1,69 @@
 package br.com.cursojsf;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+import javax.faces.bean.SessionScoped;
+import javax.faces.bean.ViewScoped;
+import javax.faces.component.html.HtmlCommandButton;
 
 @ManagedBean(name = "pessoaBean")
+@SessionScoped
 public class PessoaBean {
 
-	private String nome;
-	private String sobrenome;
+	private String nome;	
+	private String senha;
+	private String texto;
+	private List<String> nomes = new ArrayList<String>();
+	private HtmlCommandButton htmlCommandButton;
 	
 	
+	
+	
+	
+	
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+
+	public String getTexto() {
+		return texto;
+	}
+
+	public void setTexto(String texto) {
+		this.texto = texto;
+	}
+
+	public HtmlCommandButton getHtmlCommandButton() {
+		return htmlCommandButton;
+	}
+
+	public void setHtmlCommandButton(HtmlCommandButton htmlCommandButton) {
+		this.htmlCommandButton = htmlCommandButton;
+	}
+
+	public String addNome() {
+		nomes.add(nome);
+		if(nomes.size() > 2) {
+			htmlCommandButton.setDisabled(true);
+			return "paginanavegada?faces-redirect=true";
+		}
+		return "";
+	}
+	
+	public List<String> getNomes() {
+		return nomes;
+	}
+
+	public void setNomes(List<String> nomes) {
+		this.nomes = nomes;
+	}	
 
 	public String getNome() {
 		return nome;
@@ -18,12 +73,6 @@ public class PessoaBean {
 		this.nome = nome;
 	}
 
-	public String getSobrenome() {
-		return sobrenome;
-	}
-
-	public void setSobrenome(String sobrenome) {
-		this.sobrenome = sobrenome;
-	}
+	
 
 }
