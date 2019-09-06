5ae0784725b849985d4ce7e2bd9463b68ff78a74
Jefferson Brandao <jeffersonbraster@gmail.com>
2019-09-05 22:29:22 -0300

    feio melhoris na pagina do crud e adicionado novos campos

--- src/main/java/entidades/Pessoa.java
+++ src/main/java/entidades/Pessoa.java
@@ -12,8 +12,6 @@ import javax.persistence.TemporalType;
 
 @Entity
 public class Pessoa implements Serializable {
-	
-
 
 	private static final long serialVersionUID = 1L;
 	@Id
@@ -22,51 +20,154 @@ public class Pessoa implements Serializable {
 	private String nome;
 	private String sobrenome;
 	private Integer idade;
-	
+
 	@Temporal(TemporalType.DATE)
 	private Date datanascimento;
+
+	private String sexo;
+	
+	private String[] frameworks;
+	
+	private Boolean ativo;
+	
+	private String login;
+	private String senha;
+	
+	private String perfilUser;
 	
 	
-	public Pessoa() {
-		
+
+	public String getPerfilUser() {
+		return perfilUser;
 	}
+
+
+
+
+
+	public void setPerfilUser(String perfilUser) {
+		this.perfilUser = perfilUser;
+	}
+
+
+
+
+
+	public Pessoa() {
+
+	}
+
 	
 	
 	
 	
+	public String getLogin() {
+		return login;
+	}
+
+
+
+
+
+	public void setLogin(String login) {
+		this.login = login;
+	}
+
+
+
+
+
+	public String getSenha() {
+		return senha;
+	}
+
+
+
+
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+
+
+
+
+
+	public Boolean getAtivo() {
+		return ativo;
+	}
+
+
+
+	public void setAtivo(Boolean ativo) {
+		this.ativo = ativo;
+	}
+
+
+
+	public String[] getFrameworks() {
+		return frameworks;
+	}
+
+
+
+	public void setFrameworks(String[] frameworks) {
+		this.frameworks = frameworks;
+	}
+
+
+
+	public String getSexo() {
+		return sexo;
+	}
+
+	public void setSexo(String sexo) {
+		this.sexo = sexo;
+	}
+
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
+
 	public Long getId() {
 		return id;
 	}
+
 	public void setId(Long id) {
 		this.id = id;
 	}
+
 	public String getNome() {
 		return nome;
 	}
+
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
+
 	public String getSobrenome() {
 		return sobrenome;
 	}
+
 	public void setSobrenome(String sobrenome) {
 		this.sobrenome = sobrenome;
 	}
+
 	public Integer getIdade() {
 		return idade;
 	}
+
 	public void setIdade(Integer idade) {
 		this.idade = idade;
 	}
+
 	public Date getDatanascimento() {
 		return datanascimento;
 	}
+
 	public void setDatanascimento(Date datanascimento) {
 		this.datanascimento = datanascimento;
 	}
-
-
-
 
 	@Override
 	public int hashCode() {
@@ -75,9 +176,6 @@ public class Pessoa implements Serializable {
 		result = prime * result + ((id == null) ? 0 : id.hashCode());
 		return result;
 	}
-
-
-
 
 	@Override
 	public boolean equals(Object obj) {
@@ -95,15 +193,5 @@ public class Pessoa implements Serializable {
 			return false;
 		return true;
 	}
-
-
-
-
-	
-	
-	
-	
-	
-	
 
 }
