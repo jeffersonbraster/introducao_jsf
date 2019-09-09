db2f500d20e76d54c541b0ec45af735c50f5f06b
Jefferson Brandao <jeffersonbraster@gmail.com>
2019-09-09 18:31:36 -0300

    Realizado controle de acesso

--- src/main/java/br/com/cursojsf/PessoaBean.java
+++ src/main/java/br/com/cursojsf/PessoaBean.java
@@ -83,13 +83,21 @@ public class PessoaBean {
 			//adicionar o usuário na sessão
 			FacesContext context = FacesContext.getCurrentInstance();
 			ExternalContext externalContext = context.getExternalContext();
-			externalContext.getSessionMap().put("usuarioLogado", pessoaUser.getLogin());
+			externalContext.getSessionMap().put("usuarioLogado", pessoaUser);
 			
 			
 			return "primeirapagina.jsf";
 		}
 		
 		return "index.jsf";
+	}
+	
+	public boolean permiteAcesso(String acesso) {
+		FacesContext context = FacesContext.getCurrentInstance();
+		ExternalContext externalContext = context.getExternalContext();
+		Pessoa pessoaUser = (Pessoa) externalContext.getSessionMap().get("usuarioLogado");
+		
+		return pessoaUser.getPerfilUser().equals(acesso);
 	}
 
 	
